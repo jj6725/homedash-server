fc99f3b780a2cc5b3e6140a4ba6f9235ec791124
jj6725 <mengdaq@gmail.com>
2022-06-25 19:51:25 -0500

    Adding in basic influx query

--- src/index.ts
+++ src/index.ts
@@ -2,40 +2,60 @@ import express, { Express, Request, Response } from 'express';
 import axios from 'axios';
 
 const app: Express = express();
-const port = 8080;
+const port = 9001;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Server Running');
 });
 
+interface SensorMeasurement {
+  time: string
+  server: string
+  temperature: number
+  humidity: number
+}
+
 app.get('/data', (req: Request, res: Response) => {
   axios
-    .get('http://localhost:8086/query', {
+    .get('http://piserver:8086/query', {
       params: {
         db: "telegraf",
-        q: "SELECT \"temperature\"*9/5+32,\"humidity\" FROM \"pi-sensors\" GROUP BY \"url\" ORDER BY \"time\" DESC LIMIT 1"
+        q: "SELECT MEAN(\"temperature\")*9/5+32,MEAN(\"humidity\") FROM \"pi-sensors\" WHERE \"time\" > now() - 1d GROUP BY \"url\",time(1h)"
       }
     })
     .then((response) => {
       const values = response.data.results[0].series
-      const result = values.map((val: SeriesData) => {
-        return {
-          "server": val.tags.url,
-          "temperature": val.values[0][1],
-          "humidity": val.values[0][2]
-        }
+      const result: SensorMeasurement[] = values.flatMap((val: SeriesData) => {
+        const re = /pi[0-9]+/
+        const found = val.tags.url.match(re) ?? [val.tags.url]
+        return val.values.flatMap(v => {
+            return {
+              server: found[0],
+              time: v[0],
+              temperature: v[1],
+              humidity: v[2]
+            }
+          })
       })
+
+      // const newRes = result.reduce((group: TimeGroup, measurement) => {
+      //   const { time } = measurement;
+      //   group[time] = group[time] ?? [];
+      //   group[time].push(measurement);
+      //   return group;
+      // }, {});
+      
 
       if(!result) {
         res.status(500)
-        res.send("Error fetching data")
+        res.send(response)
       } else {
         res.send(result);
       }      
     })
     .catch((error) => {
       res.status(500)
-      res.send("Error fetching data")
+      res.send(error)
     })
     .then();
 });
