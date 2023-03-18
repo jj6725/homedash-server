6c7cd4bc4a6fd9172c5f9fee15b750c69e407b2c
jj6725 <mengdaq@gmail.com>
2023-03-18 17:47:16 -0500

    Replacing old data fetching with new flux query

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,17 @@
-import express, { Express, Request, Response } from 'express'
+import express, { Express, Response } from 'express'
 import axios from 'axios'
 import cors from 'cors'
+import { getData, Measurement } from './influx'
 
 const app: Express = express()
 app.use(cors())
 const port = 9001
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_, res: Response) => {
   res.send('Server Running')
 })
 
-interface SensorMeasurement {
-  time: string
-  server: string
-  temperature: number
-  humidity: number
-}
-
-app.get('/data', (req: Request, res: Response) => {
-  axios
-    .get('http://piserver:8086/query', {
-      params: {
-        db: 'telegraf',
-        q: 'SELECT MEAN("temperature")*9/5+32,MEAN("humidity") FROM "pi-sensors" WHERE "time" > now() - 2d GROUP BY "url",time(1h) FILL(0)',
-      },
-    })
-    .then((response) => {
-      const values = response.data.results[0].series
-      const result: SensorMeasurement[] = values.flatMap((val: SeriesData) => {
-        const re = /pi[0-9]+/
-        const found = val.tags.url.match(re) ?? [val.tags.url]
-        return val.values.flatMap((v) => {
-          return {
-            server: found[0],
-            time: v[0],
-            temperature: v[1],
-            humidity: v[2],
-          }
-        })
-      })
-
-      if (!result) {
-        res.status(500)
-        res.send(response)
-      } else {
-        res.send(result)
-      }
-    })
-    .catch((error) => {
-      res.status(500)
-      res.send(error)
-    })
-    .then()
-})
-
-app.get('/status', (req: Request, res: Response) => {
+app.get('/status', (_, res: Response) => {
   const requests = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
     .map((n) => 'pi' + n)
     .map((pi) => {
@@ -78,6 +35,27 @@ app.get('/status', (req: Request, res: Response) => {
   Promise.all(requests).then((result) => {
     res.send(result)
   })
+})
+
+app.get('/data', (_, res: Response) => {
+  getData([
+    Measurement.AQ_pm03,
+    Measurement.AQ_pm05,
+    Measurement.AQ_pm10,
+    Measurement.AQ_pm25,
+    Measurement.AQ_pm50,
+    Measurement.AQ_pm100,
+    Measurement.Temperature,
+    Measurement.Humidity,
+  ]).then(
+    (result) => {
+      res.send(result)
+    },
+    (error) => {
+      res.status(500)
+      res.send(error)
+    }
+  )
 })
 
 app.listen(port, () => {
--- src/influx.ts
+++ src/influx.ts
@@ -1,8 +1,83 @@
-interface SeriesData {
-  name: string
-  tags: {
-    url: string
-  }
-  columns: string[]
-  values: any[]
+import { InfluxDB } from '@influxdata/influxdb-client'
+
+type RawDataRow = {
+  result: string
+  table: number
+  _start: string
+  _stop: string
+  _time: string
+  _value: number
+  _field: string
+  _measurement: string
+  url: string
+}
+
+export enum Measurement {
+  Temperature = 'temp',
+  Humidity = 'humidity',
+  Gas = 'gas',
+  Lux = 'lux',
+  Pressure = 'pressure',
+  VOC = 'voc_index',
+  AQ_pm03 = 'air_quality_pm03',
+  AQ_pm05 = 'air_quality_pm05',
+  AQ_pm10 = 'air_quality_pm10',
+  AQ_pm25 = 'air_quality_pm100',
+  AQ_pm50 = 'air_quality_pm25',
+  AQ_pm100 = 'air_quality_pm50',
+  Unknown = ' default',
+}
+
+type SingleValue = {
+  server: string
+  value: number
+}
+
+type MeasurementMap = {
+  [index: string]: SingleValue[]
+}
+
+// index is date
+type ParsedResult = {
+  [index: string]: MeasurementMap
+}
+
+const token = ''
+const queryApi = new InfluxDB({ url: 'http://pihome:8086', token }).getQueryApi('pihome')
+const fluxQueryBase =
+  'from(bucket: "homedata") |> range(start: -48h) |> filter(fn: (r) => r["_measurement"] == "pi-sensors")'
+const fluxQueryAggregate = ' |> aggregateWindow(every: 1h, fn: mean, createEmpty: false) |> yield(name: "mean")'
+
+function generateQuery(measurements: Measurement[]): string {
+  const fields = measurements.map((m) => `r["_field"] == "${m}"`)
+  const fluxQueryFields = `|> filter(fn: (r) => ${fields.join(' or ')} )`
+  return fluxQueryBase + fluxQueryFields + fluxQueryAggregate
+}
+
+export async function getData(measurements: Measurement[]): Promise<ParsedResult> {
+  const query = generateQuery(measurements)
+  console.log('Running query: ' + query)
+  const data = await queryApi.collectRows<RawDataRow>(query)
+
+  const dates = data.map((row) => row._time)
+  const uniqueDates = [...new Set(dates)]
+
+  const result: ParsedResult = {}
+  uniqueDates.forEach((date) => {
+    const mMap: MeasurementMap = {}
+    measurements.forEach((field) => {
+      mMap[field] = data
+        .filter((r) => r._time === date)
+        .filter((r) => r._field === field)
+        .map((r) => {
+          return {
+            server: new URL(r.url).hostname,
+            value: r._value,
+          }
+        })
+    })
+    result[date] = mMap
+  })
+
+  return result
 }
